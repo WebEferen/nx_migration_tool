53b082576f16d1dda48641971ea2f3da1d93f2c4
Michal Makowski <michal.bartlomiej.makowski@nexontis.com>
2021-09-08 16:45:51 +0200

    Added file generator

--- tools/generators/migration/utils/use-command.ts
+++ tools/generators/migration/utils/use-command.ts
@@ -3,7 +3,7 @@ import { spawn } from 'cross-spawn';
 export async function useCommand(commandName: string, commandArgs: string[] = []) {
     const child = spawn(commandName, commandArgs);
     return new Promise<{ success: boolean }>((resolve) => {
-        // child.stdout.on('data', (data) => process.stdout.write(data));
+        child.stdout.on('data', (data) => process.stdout.write(data));
         child.stderr.on('data', (data) => process.stderr.write(data));
 
         child.on('close', (code) => resolve({ success: code === 0 }));
